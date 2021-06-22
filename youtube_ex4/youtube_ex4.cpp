70b33178c791e6d708464fb2c249f40c4c0ec7f0
Bjorn Chrisnach <41198499+BjornChrisnach@users.noreply.github.com>
2021-06-22 23:34:29 +0200

    First commit from vs code, finished 8 lessons, added 4 projects with builds from the cli so a.exe io projectName.exe, readme.md and .gitignore

---
+++ youtube_ex4/youtube_ex4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int slices;
+    cout << "Yo fatty how many pieces of pizza you eat?: ";
+    cin >> slices;
+    cout << "You have "<< slices << " slices of pizza." << endl;
+
+    return 0;
+}
